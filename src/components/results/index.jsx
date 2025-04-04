4585a115b7b6f3d9c57e85838fc8e49e303591d5
Danielle Luz <daniln2209@gmail.com>
2025-04-04 00:11:56 -0300

    feat: styled results text

--- src/components/results/index.jsx
+++ src/components/results/index.jsx
@@ -1,10 +1,19 @@
+import "./index.css";
+
 const Results = () => {
   return (
-    <section>
-      <h2>Sustainability results</h2>
-      <p>
-        At EcoTech Solutions, we turn promises into action. Every product sold builds a healthier planet.
-      </p>
+    <section className="results">
+      <article className="results-text">
+        <h2 className="results-title">Sustainability results</h2>
+        <p className="results-description">
+          At EcoTech Solutions, we turn promises into action. Every product sold builds a healthier planet.
+        </p>
+      </article>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </section>
   )
 }
