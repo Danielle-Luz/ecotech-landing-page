e499f7959cffce38ec303dc6f329e99ba588f2cc
Danielle Luz <daniln2209@gmail.com>
2025-04-03 23:54:16 -0300

    feat: created results component

---
+++ src/components/results/index.jsx
@@ -0,0 +1,12 @@
+const Results = () => {
+  return (
+    <section>
+      <h2>Sustainability results</h2>
+      <p>
+        At EcoTech Solutions, we turn promises into action. Every product sold builds a healthier planet.
+      </p>
+    </section>
+  )
+}
+
+export default Results;
