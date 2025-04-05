d3f8c2bc0bccef44a050ffb52b10db70e3d7df04
Danielle Luz <daniln2209@gmail.com>
2025-04-04 23:56:07 -0300

    feat: create products array

---
+++ src/data/products.js
@@ -0,0 +1,24 @@
+const products = [
+  {
+    image: "./../../../public/assets/solar-charger.svg",
+    title: "Portable Solar Charger 100W",
+    description: "Lightweight and efficient, the EcoPower 100W can be used to charge your devices and  reduce your carbon footprint with clean, renewable energy."
+  },
+  {
+    image: "./../../../public/assets/eco-light.svg",
+    title: "Solar Garden Light EcoLight",
+    description: "The EcoLight solar garden light is ideal for illuminating gardens, it  provides up to 12 hours of continuous light after a day of charging."
+  },
+  {
+    image: "./../../../public/assets/house-with-solar-panels.svg",
+    title: "Home Solar Energy Kit EcoHome",
+    description: "Reduce your electricity bill by up to 90% with the EcoHome kit. Includes solar panels, an inverter, and a high-capacity battery."
+  },
+  {
+    image: "./../../../public/assets/solar-bag.svg",
+    title: "Solar-Powered Backpack EcoBag",
+    description: "The EcoBag backpack combines ergonomic design with an integrated 20W solar panel. Perfect for hiking, traveling, or daily use."
+  }
+];
+
+export default products;
