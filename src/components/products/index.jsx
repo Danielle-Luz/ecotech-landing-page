324b8c0b9222430711817ec68f3487f95753f9ea
Danielle Luz <daniln2209@gmail.com>
2025-04-06 14:21:15 -0300

    feat: styled products section and added side gradient

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -1,14 +1,15 @@
+import "./index.css";
 import products from "../../data/products";
 
 const Products = () => {
   return (
-    <section className="horizontal-spacing-1 products">
+    <section id="products" className="horizontal-spacing-1 products">
       <h2 className="title-2">Our products</h2>
-      <ul>
+      <ul className="products-cards">
         {
           products.map((product, index) => {
             return (
-              <li key={index}>
+              <li className="product-card-item" key={index}>
                 <img className="product-image" src={product.image} />
                 <h3 className="title-3">{product.title}</h3>
                 <p className="text-2">{product.description}</p>
