befe2a8b3d448dbe79211baa45938f52de34142d
Danielle Luz <daniln2209@gmail.com>
2025-04-06 13:29:19 -0300

    feat: separate texts and spacing styles into specific classes

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -2,7 +2,14 @@ import products from "../../data/products";
 
 const Products = () => {
   return (
-    <></>
+    <section className="products">
+      <h2>Our products</h2>
+      <ul>
+        <li>
+
+        </li>
+      </ul>
+    </section>
   )
 }
 
