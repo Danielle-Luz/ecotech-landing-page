2ba8f0636b9dd84fab7f0e1970ba7d060db6175a
Danielle Luz <daniln2209@gmail.com>
2025-04-06 14:49:08 -0300

    feat: created testimonials data

--- src/components/testimonials/index.jsx
+++ src/components/testimonials/index.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 const Testimonials = () => {
   return (
     <section id="testimonials" className="testimonials">
-      <article className="testimonials-text">
+      <article className="horizontal-spacing-1 testimonials-text">
         <h2 className="title-2">Real Stories, Real Impact</h2>
         <p className="text-2">
           See how EcoTech is powering a greener future—straight from those who’ve made the switch.
