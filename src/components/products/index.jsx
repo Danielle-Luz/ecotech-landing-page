6be7f3a2981755c748e30590a6b2adfe8b6254c7
Danielle Luz <daniln2209@gmail.com>
2025-04-06 13:33:33 -0300

    feat: added products section structure

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -2,12 +2,20 @@ import products from "../../data/products";
 
 const Products = () => {
   return (
-    <section className="products">
-      <h2>Our products</h2>
+    <section className="horizontal-spacing-1 products">
+      <h2 className="title-2">Our products</h2>
       <ul>
-        <li>
-
-        </li>
+        {
+          products.forEach((product, index) => {
+            return (
+              <li key={index}>
+                <img className="product-image" src={product.image} />
+                <h3 className="title-3">{product.title}</h3>
+                <p className="text-2">{product.description}</p>
+              </li>
+            )
+          })
+        }
       </ul>
     </section>
   )
