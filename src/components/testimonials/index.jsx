1a7e02697c69c0ed64ce6f873c301c270eb9b043
Danielle Luz <daniln2209@gmail.com>
2025-04-06 23:33:28 -0300

    feat: added testimonials and styled them

--- src/components/testimonials/index.jsx
+++ src/components/testimonials/index.jsx
@@ -1,4 +1,5 @@
 import "./index.css";
+import testimonials from "../../data/testimonials";
 
 const Testimonials = () => {
   return (
@@ -9,6 +10,24 @@ const Testimonials = () => {
           See how EcoTech is powering a greener future—straight from those who’ve made the switch.
         </p>
       </article>
+      <ul className="horizontal-spacing-1 testimonials-cards">
+        {
+          testimonials.map((testimonial, index) => {
+            return (
+              <li key={index} className="testimonial-card-item">
+                <article className="all-sides-spacing-1 testimonial-content">
+                  <img className="testimonial-image" src={testimonial.image} />
+                  <p className="text-2">{testimonial.content}</p>
+                </article>
+                <article className="all-sides-spacing-1 testimonial-footer">
+                  <p className="title-3">{testimonial.author}</p>
+                  <p className="text-2">{testimonial.location}</p>
+                </article>
+              </li>
+            )
+          })
+        }
+      </ul>
     </section>
   );
 }
