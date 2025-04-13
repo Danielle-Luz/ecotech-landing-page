d8d5b2af5d5eb8ec8c0d76d38863c35e838a8b01
Danielle Luz <daniln2209@gmail.com>
2025-04-13 17:44:33 -0300

    feat: added scroll button, section and separated product item

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -1,23 +1,43 @@
 import "./index.css";
 import products from "../../data/products";
+import { useRef } from "react";
+import ProductItem from "../product-item";
+
 
 const Products = () => {
+  const productsList = useRef();
+
+  const scrollDirection = {
+    left: -1,
+    right: 1
+  }
+
+  const scrollProducts = (horizontalAxis) => {
+    const scrollAmount = 300;
+    productsList.current.scrollTo(scrollAmount * horizontalAxis, 0);
+  } 
+
   return (
-    <section id="products" className="horizontal-spacing-1 products">
+    <section id="products" className="horizontal-spacing-2 products">
       <h2 className="title-2">Our products</h2>
-      <ul className="products-cards">
+      <div className="products-card-wrapper">
+        <button onClick={() => scrollProducts(scrollDirection.left)}>
+          <img className="product-arrow mirror" src="https://img.icons8.com/ios/333333/300/circled-chevron-right--v1.png" />
+        </button>
+        <ul className="products-cards" ref={productsList}>
         {
           products.map((product, index) => {
             return (
-              <li className="product-card-item" key={index}>
-                <img className="product-image" src={product.image} />
-                <h3 className="title-3">{product.title}</h3>
-                <p className="text-2">{product.description}</p>
-              </li>
+              <ProductItem key={index} product={product} />
             )
           })
         }
-      </ul>
+        </ul>
+        <button onClick={() => scrollProducts(scrollDirection.right)}>
+          <img className="product-arrow" src="https://img.icons8.com/ios/333333/300/circled-chevron-right--v1.png" />
+        </button>
+      </div>
+      
     </section>
   )
 }
