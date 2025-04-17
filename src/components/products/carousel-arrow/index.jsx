600938dbf8047eb11b89c59f2ad921573e03cb1c
Danielle Luz <daniln2209@gmail.com>
2025-04-17 00:23:27 -0300

    feat: added arrows on carousel and styled them

--- src/components/products/carousel-arrow/index.jsx
+++ src/components/products/carousel-arrow/index.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
 import "./index.css";
 
-const CarouselArrow = ({previousArrow}) => {
+const CarouselArrow = ({previousArrow, onClick}) => {
   return (
-    <button>
+    <button onClick={onClick}>
       <img 
         className={`carousel-arrow ${previousArrow ? "mirror": ""} `} 
         src="https://img.icons8.com/ios/333333/300/circled-chevron-right--v1.png" 
--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import products from "../../data/products";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import CarouselArrow from "./carousel-arrow";
 
 
 const Products = () => {
@@ -11,43 +12,43 @@ const Products = () => {
       <h2 className="title-2">Our products</h2>
       <div  className="slider-container">
         <Slider
-        dots= {true}
-        dotsClass="slick-dots slick-thumb"
-        infinite= {true}
-        speed={500}
-        slidesToShow={4}
-        slidesToScroll={4}
-        initialSlide={0}
-        arrows={true}
-        responsive={[
-          {
-            breakpoint: 1100,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+          arrows={true}
+          infinite= {true}
+          speed={500}
+          slidesToShow={4}
+          slidesToScroll={4}
+          initialSlide={0}
+          nextArrow={<CarouselArrow to="next" />}
+          prevArrow={<CarouselArrow to="prev" previousArrow={true} />}
+          responsive={[
+            {
+              breakpoint: 1500,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 1250,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 880,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
             }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]}
+          ]}
         >
           {
             products.map((product, index) => {
               return (
-                <li key={index}>
+                <li className="product-card-item" key={index}>
                   <img className="product-image" src={product.image} />
                   <h3 className="title-3">{product.title}</h3>
                   <p className="text-2">{product.description}</p>
