8df2406be526712a10e7e7d3eeb156ac6a2b615d
Danielle Luz <daniln2209@gmail.com>
2025-04-16 23:36:35 -0300

    feat: created carousel

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -1,41 +1,61 @@
+import Slider from "react-slick";
 import "./index.css";
 import products from "../../data/products";
-import { useRef } from "react";
-import ProductItem from "../product-item";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 const Products = () => {
-  const productsList = useRef();
-
-  const scrollDirection = {
-    left: -1,
-    right: 1
-  }
-
-  const scrollProducts = (horizontalAxis) => {
-    const scrollAmount = 300;
-    productsList.current.scrollTo(scrollAmount * horizontalAxis, 0);
-  } 
-
   return (
     <section id="products" className="horizontal-spacing-2 products">
       <h2 className="title-2">Our products</h2>
-      <div className="products-card-wrapper">
-        <button onClick={() => scrollProducts(scrollDirection.left)}>
-          <img className="product-arrow mirror" src="https://img.icons8.com/ios/333333/300/circled-chevron-right--v1.png" />
-        </button>
-        <ul className="products-cards" ref={productsList}>
-        {
-          products.map((product, index) => {
-            return (
-              <ProductItem key={index} product={product} />
-            )
-          })
-        }
-        </ul>
-        <button onClick={() => scrollProducts(scrollDirection.right)}>
-          <img className="product-arrow" src="https://img.icons8.com/ios/333333/300/circled-chevron-right--v1.png" />
-        </button>
+      <div  className="slider-container">
+        <Slider
+        dots= {true}
+        dotsClass="slick-dots slick-thumb"
+        infinite= {true}
+        speed={500}
+        slidesToShow={4}
+        slidesToScroll={4}
+        initialSlide={0}
+        arrows={true}
+        responsive={[
+          {
+            breakpoint: 1100,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]}
+        >
+          {
+            products.map((product, index) => {
+              return (
+                <li key={index}>
+                  <img className="product-image" src={product.image} />
+                  <h3 className="title-3">{product.title}</h3>
+                  <p className="text-2">{product.description}</p>
+                </li>
+              )
+            })
+          }
+        </Slider>
       </div>
       
     </section>
